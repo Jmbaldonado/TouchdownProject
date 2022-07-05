d23767fa6b921334f1efbb96538aad11b7529ae4
John Michael Baldonado <jm.baldonado@whitecloak.com>
2022-07-05 12:18:46 +0800

    Add assets and create FooterView

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by John Michael Baldonado on 7/5/22.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
