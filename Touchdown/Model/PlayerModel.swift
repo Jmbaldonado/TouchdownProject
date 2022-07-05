2e24c6f54a133cdd20286009fd23f71f3f340716
John Michael Baldonado <jm.baldonado@whitecloak.com>
2022-07-05 13:33:52 +0800

    Create carousel with Page Tab View

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by John Michael Baldonado on 7/5/22.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
