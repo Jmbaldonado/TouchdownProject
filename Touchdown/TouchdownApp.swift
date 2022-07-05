2604331356d23d547f031a09577da2f34d4e249c
John Michael Baldonado <jm.baldonado@whitecloak.com>
2022-07-05 11:57:21 +0800

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by John Michael Baldonado on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
